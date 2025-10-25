e52f7fb156b13d6eb7af79906d717cec4fec9146
Hellicat <jhccop@naver.com>
2025-10-25 23:46:10 +0900

    📝 First Commit
    first Commit

---
+++ src/main/java/com/cookrep_spring/app/CookrepSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cookrep_spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookrepSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookrepSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cookrep_spring/app/CookrepSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookrep_spring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookrepSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
