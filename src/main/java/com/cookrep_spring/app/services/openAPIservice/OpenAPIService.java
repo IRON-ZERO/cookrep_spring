745b98ea334bfb2ab9cf20df9fcec7d0266eed2e
Hellicat <jhccop@naver.com>
2025-11-17 15:24:29 +0900

    🔥 [Fix] Hot FIX security
    [Fix] Hot FIX security

---
+++ src/main/java/com/cookrep_spring/app/services/openAPIservice/OpenAPIService.java
@@ -0,0 +1,13 @@
+package com.cookrep_spring.app.services.openAPIservice;
+
+import org.springframework.stereotype.Service;
+
+import com.cookrep_spring.app.dto.openAPI.OpenAPIDto;
+
+@Service
+public class OpenAPIService {
+
+	public OpenAPIDto get() {
+		return null;
+	}
+}
